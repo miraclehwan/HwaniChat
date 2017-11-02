76597d58086ddfd7f8465451ed0d9d2705409275
daehwan.kim <daehwan7298@gmail.com>
2017-11-02 15:16:26 +0900

    second

--- app/src/main/java/daehwan/com/hwanitalk/MainActivity.java
+++ app/src/main/java/daehwan/com/hwanitalk/MainActivity.java
@@ -2,14 +2,19 @@ package daehwan.com.hwanitalk;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.net.URISyntaxException;
+import java.util.ArrayList;
 
 import io.socket.client.IO;
 import io.socket.client.Socket;
@@ -17,15 +22,10 @@ import io.socket.emitter.Emitter;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Socket mSocket;
-    {
-        try{
-            String URL = "";
-            mSocket = IO.socket(URL);
-        }catch (URISyntaxException e){}
-    }
+    ArrayList<Recycler_Chat_item> items = new ArrayList<>();
+    Recycler_Chat_Adapter adapter;
 
-    TextView getMessageTextView;
+    RecyclerView getMessageRecyclerView;
     EditText sendMessageTextView;
     Button sendMessageButton;
 
@@ -37,10 +37,13 @@ public class MainActivity extends AppCompatActivity {
 
         mSocket.connect();
 
-        getMessageTextView = (TextView) findViewById(R.id.getmessageTextView);
+        getMessageRecyclerView = (RecyclerView) findViewById(R.id.getMessageRecyclerView);
         sendMessageTextView = (EditText) findViewById(R.id.sendmessageTextView);
         sendMessageButton = (Button) findViewById(R.id.sendmessageButton);
 
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        getMessageRecyclerView.setHasFixedSize(true);
+        getMessageRecyclerView.setLayoutManager(layoutManager);
 
         //Send Button Event
         sendMessageButton.setOnClickListener(sendMessage);
@@ -58,15 +61,25 @@ public class MainActivity extends AppCompatActivity {
         mSocket.disconnect();
     }
 
+    private Socket mSocket;
+    {
+        try{
+            String URL = "";
+            mSocket = IO.socket(URL);
+        }catch (URISyntaxException e){}
+    }
+
     Emitter.Listener getMessage = new Emitter.Listener() {
         @Override
         public void call(final Object... args) {
-            runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    getMessageTextView.setText(String.valueOf(args[0]));
-                }
-            });
+            try {
+                JSONObject jsonObject = (JSONObject) args[0];
+                items.add(new Recycler_Chat_item(jsonObject.getString("id"), jsonObject.getString("message")));
+                adapter = new Recycler_Chat_Adapter(getApplicationContext(), items, R.layout.activity_main);
+                getMessageRecyclerView.setAdapter(adapter);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
         }
     };
 
